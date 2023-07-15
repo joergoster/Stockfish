373f8ca6e6ff6ebf388175b19ff221d4d5aa0dca
Joerg Oster <osterj165@googlemail.com>
2023-07-15 10:53:48 +0200

    Default bonus. Bench: 4638328

--- src/search.cpp
+++ src/search.cpp
@@ -81,7 +81,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min(336 * d - 547, 1561) / 4;
+    return std::min(336 * d - 547, 1561);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
