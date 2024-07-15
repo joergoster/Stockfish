74d7670931831b207dc926fc9bc4147b3ce3ed56
J. Oster <osterj165@googlemail.com>
2024-07-15 19:13:02 +0200

    Finally, change and update copyright notice.

--- src/benchmark.h
+++ src/benchmark.h
@@ -1,13 +1,17 @@
 /*
+  Matefish, a UCI chess engine for solving mates derived from Stockfish.
   Stockfish, a UCI chess playing engine derived from Glaurung 2.1
-  Copyright (C) 2004-2024 The Stockfish developers (see AUTHORS file)
+  Copyright (C) 2004-2008 Tord Romstad (Glaurung author)
+  Copyright (C) 2008-2015 Marco Costalba, Joona Kiiski, Tord Romstad
+  Copyright (C) 2015-2024 The Stockfish developers (see AUTHORS file)
+  Copyright (C) 2021-2024 Jörg Oster
 
-  Stockfish is free software: you can redistribute it and/or modify
+  Matefish is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  Stockfish is distributed in the hope that it will be useful,
+  Matefish is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
