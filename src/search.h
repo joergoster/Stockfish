7925f113a76415361c61a4aed12a247a71ba3948
J. Oster <osterj165@googlemail.com>
2025-07-13 16:21:10 +0200

    Reduce output. Fail-highs and -lows will now be sent once every second. Current move and number every 200 milliseconds, yet in MultiPV mode, this info will only be sent after the last PV line.
    No perfect solution, but it should noticeably ease the burden
    of the UI.
    
    No functional change.

--- src/search.h
+++ src/search.h
@@ -247,7 +247,8 @@ class SearchManager: public ISearchManager {
     double               previousTimeReduction;
     Value                bestPreviousScore;
     Value                bestPreviousAverageScore;
-    bool                 stopOnPonderhit;
+    bool                 newPVIdx, stopOnPonderhit;
+    TimePoint            lastInfoFail, lastInfoCurrmove;
 
     size_t id;
 
@@ -326,7 +327,7 @@ class Worker {
 
     LimitsType limits;
 
-    size_t                pvIdx, pvLast;
+    size_t                multiPV, pvIdx, pvLast;
     std::atomic<uint64_t> nodes, tbHits, bestMoveChanges;
     int                   selDepth, nmpMinPly;
 
