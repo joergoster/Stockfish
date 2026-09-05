6798a0bf85366cb6b8b374ab0543ba0c6dece28a
Jörg Oster <osterj165@googlemail.com>
2026-09-05 18:48:37 +0200

    Cleaner code. No functional change.

--- src/attacks.h
+++ src/attacks.h
@@ -252,8 +252,9 @@ inline constexpr auto PseudoAttacks = []() constexpr {
 
         attacks[KING][s1]   = pseudo_attacks(KING, s1);
         attacks[KNIGHT][s1] = pseudo_attacks(KNIGHT, s1);
-        attacks[QUEEN][s1] = attacks[BISHOP][s1] = pseudo_attacks(BISHOP, s1);
-        attacks[QUEEN][s1] |= attacks[ROOK][s1]  = pseudo_attacks(ROOK, s1);
+        attacks[BISHOP][s1] = pseudo_attacks(BISHOP, s1);
+        attacks[ROOK][s1]   = pseudo_attacks(ROOK, s1);
+        attacks[QUEEN][s1]  = attacks[BISHOP][s1] | attacks[ROOK][s1]
     }
 
     return attacks;
